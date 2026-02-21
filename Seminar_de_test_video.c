696f749b38a5107a8e31eca1956f97cf3486add1
radumirunaioana06 <radumirunaioana06@gmail.com>
2026-02-22 00:33:07 +0200

    Am adaugat un fisier test

---
+++ Seminar_de_test_video.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int  main() {
+
+	printf("introduceti un numar real: ");
+	float variabila = 0.0;
+	scanf_s("%f", &variabila);
+	printf("Ati introdus:%5.2f", variabila);
+
+
+}
